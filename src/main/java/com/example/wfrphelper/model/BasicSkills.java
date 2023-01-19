046239df9c8e07dbef4a87335e7b0b916d231d23
BigMaestrov <nikita.egorov40k@gmail.com>
2023-01-19 12:51:18 +0400

    add exp fields

--- src/main/java/com/example/wfrphelper/model/BasicSkills.java
+++ src/main/java/com/example/wfrphelper/model/BasicSkills.java
@@ -4,29 +4,261 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BasicSkills {
-    private int art;
-    private int athletics;
-    private int bribery;
-    private int charm;
-    private int charmAnimal;
-    private int climb;
-    private int cool;
-    private int consumeAlcohol;
-    private int dodge;
-    private int drive;
-    private int endurance;
-    private int entertain;
-    private int gamble;
-    private int gossip;
-    private int haggle;
-    private int intimidate;
-    private int Intuition;
-    private int leadership;
-    private int melee;
-    private int navigation;
-    private int outDoorSurvival;
-    private int perception;
-    private int ride;
-    private int row;
-    private int stealth;
+    //1 value - start, 2 value - numSteps
+    private int[] art;
+    private int[] athletics;
+    private int[] bribery;
+    private int[] charm;
+    private int[] charmAnimal;
+    private int[] climb;
+    private int[] cool;
+    private int[] consumeAlcohol;
+    private int[] dodge;
+    private int[] drive;
+    private int[] endurance;
+    private int[] entertain;
+    private int[] gamble;
+    private int[] gossip;
+    private int[] haggle;
+    private int[] intimidate;
+    private int[] intuition;
+    private int[] leadership;
+    private int[] melee;
+    private int[] navigation;
+    private int[] outDoorSurvival;
+    private int[] perception;
+    private int[] ride;
+    private int[] row;
+    private int[] stealth;
+
+    public BasicSkills(int[] art, int[] athletics, int[] bribery, int[] charm, int[] charmAnimal, int[] climb, int[] cool, int[] consumeAlcohol, int[] dodge, int[] drive, int[] endurance, int[] entertain, int[] gamble, int[] gossip, int[] haggle, int[] intimidate, int[] intuition, int[] leadership, int[] melee, int[] navigation, int[] outDoorSurvival, int[] perception, int[] ride, int[] row, int[] stealth) {
+        this.art = art;
+        this.athletics = athletics;
+        this.bribery = bribery;
+        this.charm = charm;
+        this.charmAnimal = charmAnimal;
+        this.climb = climb;
+        this.cool = cool;
+        this.consumeAlcohol = consumeAlcohol;
+        this.dodge = dodge;
+        this.drive = drive;
+        this.endurance = endurance;
+        this.entertain = entertain;
+        this.gamble = gamble;
+        this.gossip = gossip;
+        this.haggle = haggle;
+        this.intimidate = intimidate;
+        this.intuition = intuition;
+        this.leadership = leadership;
+        this.melee = melee;
+        this.navigation = navigation;
+        this.outDoorSurvival = outDoorSurvival;
+        this.perception = perception;
+        this.ride = ride;
+        this.row = row;
+        this.stealth = stealth;
+    }
+
+    public BasicSkills() {
+    }
+
+    public int[] getArt() {
+        return art;
+    }
+
+    public void setArt(int[] art) {
+        this.art = art;
+    }
+
+    public int[] getAthletics() {
+        return athletics;
+    }
+
+    public void setAthletics(int[] athletics) {
+        this.athletics = athletics;
+    }
+
+    public int[] getBribery() {
+        return bribery;
+    }
+
+    public void setBribery(int[] bribery) {
+        this.bribery = bribery;
+    }
+
+    public int[] getCharm() {
+        return charm;
+    }
+
+    public void setCharm(int[] charm) {
+        this.charm = charm;
+    }
+
+    public int[] getCharmAnimal() {
+        return charmAnimal;
+    }
+
+    public void setCharmAnimal(int[] charmAnimal) {
+        this.charmAnimal = charmAnimal;
+    }
+
+    public int[] getClimb() {
+        return climb;
+    }
+
+    public void setClimb(int[] climb) {
+        this.climb = climb;
+    }
+
+    public int[] getCool() {
+        return cool;
+    }
+
+    public void setCool(int[] cool) {
+        this.cool = cool;
+    }
+
+    public int[] getConsumeAlcohol() {
+        return consumeAlcohol;
+    }
+
+    public void setConsumeAlcohol(int[] consumeAlcohol) {
+        this.consumeAlcohol = consumeAlcohol;
+    }
+
+    public int[] getDodge() {
+        return dodge;
+    }
+
+    public void setDodge(int[] dodge) {
+        this.dodge = dodge;
+    }
+
+    public int[] getDrive() {
+        return drive;
+    }
+
+    public void setDrive(int[] drive) {
+        this.drive = drive;
+    }
+
+    public int[] getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(int[] endurance) {
+        this.endurance = endurance;
+    }
+
+    public int[] getEntertain() {
+        return entertain;
+    }
+
+    public void setEntertain(int[] entertain) {
+        this.entertain = entertain;
+    }
+
+    public int[] getGamble() {
+        return gamble;
+    }
+
+    public void setGamble(int[] gamble) {
+        this.gamble = gamble;
+    }
+
+    public int[] getGossip() {
+        return gossip;
+    }
+
+    public void setGossip(int[] gossip) {
+        this.gossip = gossip;
+    }
+
+    public int[] getHaggle() {
+        return haggle;
+    }
+
+    public void setHaggle(int[] haggle) {
+        this.haggle = haggle;
+    }
+
+    public int[] getIntimidate() {
+        return intimidate;
+    }
+
+    public void setIntimidate(int[] intimidate) {
+        this.intimidate = intimidate;
+    }
+
+    public int[] getIntuition() {
+        return intuition;
+    }
+
+    public void setIntuition(int[] intuition) {
+        intuition = intuition;
+    }
+
+    public int[] getLeadership() {
+        return leadership;
+    }
+
+    public void setLeadership(int[] leadership) {
+        this.leadership = leadership;
+    }
+
+    public int[] getMelee() {
+        return melee;
+    }
+
+    public void setMelee(int[] melee) {
+        this.melee = melee;
+    }
+
+    public int[] getNavigation() {
+        return navigation;
+    }
+
+    public void setNavigation(int[] navigation) {
+        this.navigation = navigation;
+    }
+
+    public int[] getOutDoorSurvival() {
+        return outDoorSurvival;
+    }
+
+    public void setOutDoorSurvival(int[] outDoorSurvival) {
+        this.outDoorSurvival = outDoorSurvival;
+    }
+
+    public int[] getPerception() {
+        return perception;
+    }
+
+    public void setPerception(int[] perception) {
+        this.perception = perception;
+    }
+
+    public int[] getRide() {
+        return ride;
+    }
+
+    public void setRide(int[] ride) {
+        this.ride = ride;
+    }
+
+    public int[] getRow() {
+        return row;
+    }
+
+    public void setRow(int[] row) {
+        this.row = row;
+    }
+
+    public int[] getStealth() {
+        return stealth;
+    }
+
+    public void setStealth(int[] stealth) {
+        this.stealth = stealth;
+    }
 }
--- src/main/java/com/example/wfrphelper/model/Experience.java
+++ src/main/java/com/example/wfrphelper/model/Experience.java
@@ -4,7 +4,61 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Experience {
-    int currentExp;
-    int spentExp;
-    int totalExp;
+    private int currentExp;
+    private int spentExp;
+    private int totalExp;
+    private int improveSteps;
+    public final int[][] tableOfImproveCost = new int[][]{
+            //Steps taken development
+            {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70},
+            //Characteristic cost for steps
+            {30, 40, 50, 70, 90, 120, 150, 190, 230, 280, 330, 390, 450, 520},
+            //Skill cost for steps
+            {15, 20, 30, 40, 60, 80, 110, 140, 180, 220, 270, 320, 380, 440}};
+
+    public Experience(int currentExp, int spentExp, int totalExp, int improveSteps) {
+        this.currentExp = currentExp;
+        this.spentExp = spentExp;
+        this.totalExp = totalExp;
+        this.improveSteps = improveSteps;
+    }
+
+    public Experience() {
+    }
+
+    public int[][] getTableOfImproveCost() {
+        return tableOfImproveCost;
+    }
+
+    public int getCurrentExp() {
+        return currentExp;
+    }
+
+    public void setCurrentExp(int currentExp) {
+        this.currentExp = currentExp;
+    }
+
+    public int getSpentExp() {
+        return spentExp;
+    }
+
+    public void setSpentExp(int spentExp) {
+        this.spentExp = spentExp;
+    }
+
+    public int getTotalExp() {
+        return totalExp;
+    }
+
+    public void setTotalExp(int totalExp) {
+        this.totalExp = totalExp;
+    }
+
+    public int getImproveSteps() {
+        return improveSteps;
+    }
+
+    public void setImproveSteps(int improveSteps) {
+        this.improveSteps = improveSteps;
+    }
 }
