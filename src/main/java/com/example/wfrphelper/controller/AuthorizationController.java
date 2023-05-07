f1211fe5c95bd0bbd7e0aac4cc7800086dd6ec03
BigMaestrov <nikita.egorov40k@gmail.com>
2023-05-07 11:53:09 +0400

    Добавил авторизацию

--- src/main/java/com/example/wfrphelper/controller/AuthorizationController.java
+++ src/main/java/com/example/wfrphelper/controller/AuthorizationController.java
@@ -1,14 +1,29 @@
 package com.example.wfrphelper.controller;
 
+import com.example.wfrphelper.model.mainClasses.Master;
+import com.example.wfrphelper.model.mainClasses.Player;
 import com.example.wfrphelper.processors.AuthorizationProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Controller
 public class AuthorizationController {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    /*
     private final AuthorizationProcessor authorizationProcessor;
 
     public AuthorizationController(AuthorizationProcessor authorizationProcessor) {
@@ -31,6 +46,72 @@ public class AuthorizationController {
             model.addAttribute("message", "Login failed!");
         }
         return "authorization.html";
+    }*/
+
+
+    @PostMapping("/authorization")
+    public String login(@RequestParam("login") String login,
+                            @RequestParam("password") String password) {
+
+        //Авторизация для мастера
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT * FROM master WHERE login = ? AND password = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, login);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return "redirect:/masterPage";
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        //Авторизация для игрока
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT * FROM player WHERE login = ? AND password = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, login);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return "redirect:/playerSheet";
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return "redirect:/authorizationFailed";
+    }
+/*
+    @PostMapping("/authorization")
+    public String login2(@RequestParam("login") String login,
+                        @RequestParam("password") String password,
+                        HttpSession session) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT is_master FROM users WHERE login = ? AND password = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, login);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                session.setAttribute("login", login);
+                return "redirect:/home";
+            } else {
+                return "redirect:/authorizationFailed";
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+            return "redirect:/authorizationFailed";
+        }
+    }
+*/
+    @GetMapping("/authorization")
+    public String showAuthorizationForm() {
+        return "authorization";
+    }
+
+    @GetMapping("/authorizationFailed")
+    public String showAuthorizationFailedForm() {
+        return "authorizationFailed";
     }
 }
 
---
+++ src/main/java/com/example/wfrphelper/services/AutorizationService.java
@@ -0,0 +1,38 @@
+package com.example.wfrphelper.services;
+
+import com.example.wfrphelper.model.mainClasses.Player;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class AutorizationService {
+    //Хранилище игроков
+    private static final Map<Integer, Player> PLAYER_REPOSITORY_MAP = new HashMap<>();
+
+    public List<Player> readAll() {
+        return new ArrayList<>(PLAYER_REPOSITORY_MAP.values());
+    }
+
+    public Player read(int id) {
+        return PLAYER_REPOSITORY_MAP.get(id);
+    }
+
+    public boolean update(Player player, int id) {
+        if (PLAYER_REPOSITORY_MAP.containsKey(id)) {
+            player.setPlayerId(id);
+            PLAYER_REPOSITORY_MAP.put(id, player);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delete(int id) {
+        return PLAYER_REPOSITORY_MAP.remove(id) != null;
+    }
+}
+
